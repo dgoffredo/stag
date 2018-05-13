5f9d1c6b9f2e1935fd23ecc8c7cc311e1abf3ae9
David Goffredo <dmgoffredo@gmail.com>
2018-05-13 15:42:42 -0400

    needed to "add" in the hook, I think

--- version.rkt
+++ version.rkt
@@ -3,4 +3,4 @@
 (provide *stag-version*)
 
 (define *stag-version*
-  "The chipper second flaky language telefrags the happy black chilled pirate while the beaver kisses the haggard maroon fantastic bird.")
+  "The brazen malignant conservative whale insults the childish chilled spacious asphalt while the book filibusters the white brisk moist lanyard.")
