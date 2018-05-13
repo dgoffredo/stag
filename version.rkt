515aba3921a1eed1db63d6968d26fa7aa2804c1c
David Goffredo <dmgoffredo@gmail.com>
2018-05-13 15:39:45 -0400

    Let's see if the hook works

---
+++ version.rkt
@@ -0,0 +1,6 @@
+#lang racket/base
+
+(provide *stag-version*)
+
+(define *stag-version*
+  "The chipper second flaky language telefrags the happy black chilled pirate while the beaver kisses the haggard maroon fantastic bird.")
